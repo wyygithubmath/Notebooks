b43a9aef4af39882a89a31170f9a8284cc000f88
wyygithubmath <wyy19860927@163.com>
2018-05-12 10:54:22 +0800

    order

---
+++ 基础/Python/format.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+
+"""
+
+import random
+
+stringFormat = '{:.6f}'.format(random.random())
+
+print(stringFormat)
+print(round(random.random(), 6))
+
---
+++ 极客项目/算法/排序.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+排序
+1. 单个数字
+    +
+2. 列表
+    + sample
+    + choice
+
+"""
+
+import random
+
+stringFormat = '{:.6f}'.format(random.random())
+
+unorderList = [random.randint(1, 100) for i in range(50)]
+
+print(stringFormat)
+print(round(random.random(), 6))
+
