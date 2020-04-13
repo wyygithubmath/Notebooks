93b4731db42d2c6eadd608fbd65810e2c355a07c
wyygithubmath <wyy19860927@163.com>
2020-04-13 23:13:56 +0800

    20200413-2

--- 极客项目/Blog/wyyblog/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from wyyblog.models import Blog
-
-# Register your models here.
-
-
-@admin.register(Blog)
-class BlogAdmin(admin.ModelAdmin):
-    list_display = ['title', 'author']
--- 极客项目/Blog/wyyblog/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import JsonResponse
-from wyyblog.models import Blog
